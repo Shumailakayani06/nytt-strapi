b4cffbab8fbc79032c019528a2c430e77e7586ae
Shumaila Kayani <shumailakayani059@gmail.com>
2022-05-31 09:41:52 +0200

    deleting server.js

--- config/env/production/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ env }) => ({
-    url: env("https://nytt-strapi.herokuapp.com/"),
-    proxy: true,
-    app: {
-      keys: env.array("APP_KEYS", ["Key1", "Key2"]),
-    },
-  });
